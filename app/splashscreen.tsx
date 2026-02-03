7a3f9a5e6a056fc00dbb9cc4dc3856da93a990f4
lawrencejr5 <oputalawrence@gmail.com>
2026-02-03 09:51:19 +0100

    added android fcm1 for push notifications, also added google services json file

---
+++ app/splashscreen.tsx
@@ -0,0 +1,27 @@
+import { useTheme } from "@/context/ThemeContext";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+const SplashScreen = () => {
+  const { theme } = useTheme();
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <StatusBar style={theme === "dark" ? "light" : "dark"} />
+
+      <Image
+        source={
+          theme === "dark"
+            ? require("../assets/images/splash-screen-black.png")
+            : require("../assets/images/splash-screen-light.png")
+        }
+        style={{ width: "100%", height: "100%" }}
+      />
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({});
